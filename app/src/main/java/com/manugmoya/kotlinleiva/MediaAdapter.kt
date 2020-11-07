e0a84e559427dba0ef817ee1e74ad12d08a797bb
Manuel Gutierrez Moya <manuel.gutierrez.moya@everis.com>
2020-11-07 20:30:57 +0100

    ejemplo de delegación de propiedades con by lazy en el adapter y con delegado en la lista del adapter

--- app/src/main/java/com/manugmoya/kotlinleiva/MediaAdapter.kt
+++ app/src/main/java/com/manugmoya/kotlinleiva/MediaAdapter.kt
@@ -4,10 +4,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.manugmoya.kotlinleiva.databinding.ViewMediaItemBinding
+import kotlin.properties.Delegates
 
 class MediaAdapter(
-    private val items: List<MediaItem>, private val listener: (MediaItem) -> Unit
+    items: List<MediaItem> = emptyList(), private val listener: (MediaItem) -> Unit
 ) : RecyclerView.Adapter<MediaAdapter.ItemViewHolder>() {
+
+    // hay que hacerla publica y var para poder modificarla desde fuera de esta clase.
+    var items: List<MediaItem> by Delegates.observable(items) { _, _, _ ->
+        notifyDataSetChanged()
+    }
 
     // Numero de items
     override fun getItemCount(): Int = items.size
@@ -26,7 +32,6 @@ class MediaAdapter(
     }
 
 
-
     inner class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
         private val binding = ViewMediaItemBinding.bind(view)
@@ -34,7 +39,7 @@ class MediaAdapter(
         fun bind(item: MediaItem) = with(binding) {
             mediaTitle.text = item.title
             mediaThumb.loadUrl(item.url)
-            mediaVideoIndicator.visibility = when(item.type){
+            mediaVideoIndicator.visibility = when (item.type) {
                 MediaItem.Type.PHOTO -> View.INVISIBLE
                 MediaItem.Type.VIDEO -> View.VISIBLE
             }
