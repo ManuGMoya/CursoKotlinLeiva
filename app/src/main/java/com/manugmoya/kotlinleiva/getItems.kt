90193da3d47fb584664d8a9e4c4d4a74f15b4476
Manuel Gutierrez Moya <manuel.gutierrez.moya@everis.com>
2020-11-08 18:35:02 +0100

    Cambiar la función getItems para añadir los elementos mediante un rango

--- app/src/main/java/com/manugmoya/kotlinleiva/getItems.kt
+++ app/src/main/java/com/manugmoya/kotlinleiva/getItems.kt
@@ -1,7 +1,18 @@
 package com.manugmoya.kotlinleiva
 
-fun getItems(): List<MediaItem> {
-    val listOf = listOf(
+fun getItems1(): MutableList<MediaItem> {
+
+    val listMedia = mutableListOf<MediaItem>()
+
+    for (i in 1..10){
+        listMedia.add(MediaItem("Title $i", "https://placekitten.com/200/200?image=$i", if (i % 3 == 0){
+            MediaItem.Type.PHOTO
+        }else{
+            MediaItem.Type.VIDEO
+        }))
+    }
+
+/*    val listOf = listOf(
         MediaItem("Title 3", "https://placekitten.com/200/200?image=3", MediaItem.Type.PHOTO),
         MediaItem("Title 4", "https://placekitten.com/200/200?image=4", MediaItem.Type.PHOTO),
         MediaItem("Title 1", "https://placekitten.com/200/200?image=1", MediaItem.Type.PHOTO),
@@ -12,6 +23,12 @@ fun getItems(): List<MediaItem> {
         MediaItem("Title 8", "https://placekitten.com/200/200?image=8", MediaItem.Type.PHOTO),
         MediaItem("Title 9", "https://placekitten.com/200/200?image=9", MediaItem.Type.PHOTO),
         MediaItem("Title 10", "https://placekitten.com/200/200?image=10", MediaItem.Type.VIDEO)
-    )
-    return listOf
+    )*/
+    return listMedia
+}
+
+fun getItems() : List<MediaItem> = (1 .. 10).map{
+    (MediaItem("Title $it", "https://placekitten.com/200/200?image=$it",
+        if (it % 3 == 0) MediaItem.Type.PHOTO else MediaItem.Type.VIDEO
+    ))
 }
